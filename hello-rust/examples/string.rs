7a22b07df1604223649dd2284908db406599f440
Iyke <eokorie1911@gmail.com>
2026-02-07 20:37:01 +0100

    string

---
+++ hello-rust/examples/string.rs
@@ -0,0 +1,8 @@
+#![allow(unused)]
+
+fn main() {
+    // strings in rust there are multiple ways of calling a string in rust
+    let s: String =  String::from("I beleive i can fly");
+    let s2: String = "im sure i can fly".to_string();
+    let length:usize = s2.len(); 
+}
---
+++ hello-rust/target/debug/examples/string-e66d6c2759f89145.d
@@ -0,0 +1,5 @@
+C:\Iyke Stuff\Coding\Rust\hello-rust\target\debug\examples\string-e66d6c2759f89145.d: examples\string.rs
+
+C:\Iyke Stuff\Coding\Rust\hello-rust\target\debug\examples\libstring-e66d6c2759f89145.rmeta: examples\string.rs
+
+examples\string.rs:
