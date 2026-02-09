9d3ba21a2ab3f2089f64dcc259f3747b2691d2c3
Iyke <eokorie1911@gmail.com>
2026-02-09 13:37:16 +0100

    learning string with rust

--- hello-rust/examples/string.rs
+++ hello-rust/examples/string.rs
@@ -2,7 +2,18 @@
 
 fn main() {
     // strings in rust there are multiple ways of calling a string in rust
-    let s: String =  String::from("I beleive i can fly");
+    let s: String =  String::from("I beleive i can fly"); // String owns the data
     let s2: String = "im sure i can fly".to_string();
     let length:usize = s2.len(); 
+
+    let s3: &str = &s2[..4];
+
+    println!("{} hmm", s3);
+
+    let s4: &str = "cow";
+
+    let s_view: String = format!("{}: {}", s4, s);
+
+    println!("{}", s_view);
 }
+
---
+++ hello-rust/target/debug/examples/string.d
@@ -0,0 +1,5 @@
+C:\Iyke Stuff\Coding\Rust\hello-rust\target\debug\examples\string.d: examples\string.rs
+
+C:\Iyke Stuff\Coding\Rust\hello-rust\target\debug\examples\string.exe: examples\string.rs
+
+examples\string.rs:
