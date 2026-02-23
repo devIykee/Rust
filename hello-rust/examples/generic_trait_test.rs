edfc6735725fe017268a2ed98c5033184c3c604a
iyke <eokorie1911@gmail.com>
2026-02-23 18:45:56 +0100

    trait bound

---
+++ hello-rust/examples/generic_trait_test.rs
@@ -0,0 +1,27 @@
+pub trait Iterator<T> { 
+    fn next(&mut self) -> Option<&T>;
+}
+
+pub struct TupleIter<T> {
+    pub tuple: (T, T, T),
+    pub next: usize,
+}
+
+pub struct VecIter<T> {
+    pub vec: Vec<T>,
+    pub next: usize,
+}
+
+impl<T> Iterator<T> for TupleIter<T> {
+    fn next(&mut self) -> Option<&T> {
+        
+    }
+}
+
+impl<T> Iterator<T> for VecIter<T> {
+    [n]?
+        }fn next(&mut self) -> Option<&T> {
+        for n in self {
+            self
+    }
+}
---
+++ hello-rust/target/debug/examples/generic_trait_test-81c05eae80656788.d
@@ -0,0 +1,5 @@
+C:\Iyke Stuff\Coding\Rust\hello-rust\target\debug\examples\generic_trait_test-81c05eae80656788.d: examples\generic_trait_test.rs
+
+C:\Iyke Stuff\Coding\Rust\hello-rust\target\debug\examples\libgeneric_trait_test-81c05eae80656788.rmeta: examples\generic_trait_test.rs
+
+examples\generic_trait_test.rs:
